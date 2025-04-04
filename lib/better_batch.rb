2fb68044b32bf47ace87082d98a6fc8ece6d9a8b
Tyler Hartland <tylerhartland7@gmail.com>
2025-04-04 09:10:42 -0500

    bundle gem better_batch

---
+++ lib/better_batch.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "better_batch/version"
+
+module BetterBatch
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/better_batch/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BetterBatch
+  VERSION = "0.1.0"
+end
