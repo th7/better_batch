86e93d8572f20471f42fbf2c5f7428889b86615a
Tyler Hartland <tylerhartland7@gmail.com>
2025-04-05 08:33:56 -0500

    wip

--- lib/better_batch/multi_enum.rb
+++ lib/better_batch/multi_enum.rb
@@ -18,11 +18,10 @@ module BetterBatch
     def enums(count, &block)
       laziers = count.times.map { LazierEnum.new }
       final = Enumerator.new do |final|
-        normies = nest(count, &block)
+        normies = nest(count, final, &block)
         normies.zip(laziers) do |normie, lazier|
           normie.each_slice(lazier.per_slice) do |items|
             lazier.each_slice_block.call(items)
-            items.each { |item| final << item }
           end
         end
       end
@@ -31,20 +30,22 @@ module BetterBatch
 
     private
 
-    def nest(count, this_count=1, previous_yielders=[], normies_yielder=nil, &block)
+    def nest(count, final_yielder, this_count=1, previous_yielders=[], normies_yielder=nil, &block)
       if normies_yielder
         if count == this_count
           normies_yielder << Enumerator.new do |y|
-            block.call(*previous_yielders, y)
+            lazier_yielder = LazierYielder.new(y, final_yielder)
+            block.call(*previous_yielders, lazier_yielder)
           end
         else
           normies_yielder << Enumerator.new do |y|
-            nest(count, this_count + 1, previous_yielders << y, normies_yielder, &block)
+            lazier_yielder = LazierYielder.new(y, final_yielder)
+            nest(count, final_yielder, this_count + 1, previous_yielders << lazier_yielder, normies_yielder, &block)
           end
         end
       else
         Enumerator.new do |y|
-          nest(count, this_count, previous_yielders, y, &block)
+          nest(count, final_yielder, this_count, previous_yielders, y, &block)
         end
       end
     end
@@ -60,6 +61,18 @@ module BetterBatch
     def each_slice(per_slice, &block)
       @per_slice = per_slice
       @each_slice_block = block
+    end
+  end
+
+  class LazierYielder
+    def initialize(wrapped_yielder, final_yielder)
+      @wrapped_yielder = wrapped_yielder
+      @final_yielder = final_yielder
+    end
+
+    def <<(item)
+      @wrapped_yielder << item
+      @final_yielder << item
     end
   end
 end
