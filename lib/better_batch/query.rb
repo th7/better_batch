28be08554c0a79d1c695f8ddadfd832b77102ef4
Tyler Hartland <tylerhartland7@gmail.com>
2025-04-13 10:23:26 -0500

    wip - allow more building options

--- lib/better_batch/query.rb
+++ lib/better_batch/query.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
+require 'anbt-sql-formatter/formatter'
+
 module BetterBatch
   class Query # rubocop:disable Metrics/ClassLength
     SELECT_TEMPLATE = <<~SQL
-      select id from (%<selected_inner>s)
+      select %<selected_returning>s from (%<selected_inner>s)
       order by ordinal
     SQL
 
@@ -15,13 +17,13 @@ module BetterBatch
         %<inserted_inner>s
       )
       %<updated_clause>s
-      select coalesce(selected.id, inserted.id) as id
+      select %<upsert_returning>s
       from selected left join inserted using(%<query_columns_text>s)
       order by selected.ordinal
     SQL
 
     SELECTED_INNER_TEMPLATE = <<~SQL
-      select %<table_name>s.%<primary_key>s as id, %<input_columns_text>s, ordinal
+      select %<selected_inner_returning>s, %<input_columns_text>s, ordinal
        from rows from (
          jsonb_to_recordset($1)
          as (%<typed_columns_text>s)
@@ -35,45 +37,81 @@ module BetterBatch
         select distinct on (%<query_columns_text>s)
           %<columns_text>s, now() as created_at, now() as updated_at
         from selected
-        where id is null
-        returning id, %<query_columns_text>s
+        where %<primary_key>s is null
+        returning %<primary_key>s, %<query_columns_text>s
     SQL
 
     UPDATED_INNER_TEMPLATE = <<~SQL
       update %<table_name>s
       set %<update_columns_text>s, updated_at = now()
-      from selected where %<table_name>s.id = selected.id
+      from selected where %<table_name>s.%<primary_key>s = selected.%<primary_key>s
     SQL
 
-    def initialize(table_name:, primary_key:, columns:, column_types:, unique_columns:)
+    def initialize(table_name:, primary_key:, columns:, column_types:, unique_columns:, returning:)
       @table_name = table_name
       @primary_key = primary_key
       @columns = columns
       @column_types = column_types
       @unique_columns = unique_columns
+      @returning = returning.nil? ? @column_types.keys : returning
     end
 
     def select
-      format(SELECT_TEMPLATE, selected_inner:)
+      format(SELECT_TEMPLATE, selected_returning:, selected_inner:)
+    end
+
+    def select_formatted
+      format_sql(select)
     end
 
     def upsert
-      params = { selected_inner:, inserted_inner:, updated_clause:, query_columns_text: }
+      params = { selected_inner:, inserted_inner:, updated_clause:, upsert_returning:, query_columns_text: }
       format(UPSERT_TEMPLATE, **params)
+    end
+
+    def upsert_formatted
+      format_sql(upsert)
+    end
+
+    def inspect
+      vars = [
+        :@table_name,
+        :@primary_key,
+        :@columns,
+        :@column_types,
+        :@unique_columns,
+        :@returning
+      ].map { |var| "#{var}=#{instance_variable_get(var)}" }
+      "#<#{self.class.name}:#{vars}>"
     end
 
     private
 
-    attr_reader :table_name, :columns, :column_types, :unique_columns, :primary_key
+    attr_reader :table_name, :columns, :column_types, :unique_columns, :primary_key, :returning
+
+    def selected_returning
+      @selected_returning ||= returning.join(', ')
+    end
 
     def selected_inner
       @selected_inner ||= build_selected_inner
     end
 
     def build_selected_inner
-      params = { table_name:, primary_key:, input_columns_text:, typed_columns_text:, columns_text:,
+      params = { table_name:, primary_key:, selected_inner_returning:, input_columns_text:, typed_columns_text:, columns_text:,
                  query_columns_text: }
       format(SELECTED_INNER_TEMPLATE, **params)
+    end
+
+    def selected_inner_returning
+      @selected_inner_returning ||= build_selected_inner_returning
+    end
+
+    def build_selected_inner_returning
+      qualified_columns = ([primary_key] + returning - columns).uniq.zip([table_name].cycle).map do |col, table|
+        "#{table}.#{col}"
+      end
+      qualified_columns.join(', ')
     end
 
     def inserted_inner
@@ -81,7 +119,7 @@ module BetterBatch
     end
 
     def build_inserted_inner
-      format(INSERTED_INNER_TEMPLATE, table_name:, columns_text:, query_columns_text:)
+      format(INSERTED_INNER_TEMPLATE, table_name:, primary_key:, columns_text:, query_columns_text:)
     end
 
     def updated_clause
@@ -106,7 +144,13 @@ module BetterBatch
     end
 
     def build_updated_inner
-      format(UPDATED_INNER_TEMPLATE, table_name:, update_columns_text:)
+      format(UPDATED_INNER_TEMPLATE, table_name:, primary_key:, update_columns_text:)
+    end
+
+    def upsert_returning
+      returning.map do |col|
+        "coalesce(selected.#{col}, inserted.#{col})"
+      end.join(', ')
     end
 
     def input_columns_text
@@ -114,7 +158,7 @@ module BetterBatch
     end
 
     def typed_columns_text
-      @typed_columns_text ||= column_types.map { |pair| pair.join(' ') }.join(', ')
+      @typed_columns_text ||= columns.map { |c| "#{c} #{column_types[c]}" }.join(', ')
     end
 
     def columns_text
@@ -131,6 +175,16 @@ module BetterBatch
 
     def update_columns_text
       @update_columns_text ||= update_columns.map { |c| "#{c} = selected.#{c}" }.join(', ')
+    end
+
+    # modified from
+    # https://github.com/sonota88/anbt-sql-formatter/blob/main/bin/anbt-sql-formatter
+    def format_sql(src)
+      rule = AnbtSql::Rule.new
+      rule.keyword = AnbtSql::Rule::KEYWORD_LOWER_CASE
+      rule.indent_string = '  '
+      formatter = AnbtSql::Formatter.new(rule)
+      formatter.format(src)
     end
   end
 end
--- spec/better_batch/query_spec.rb
+++ spec/better_batch/query_spec.rb
@@ -19,21 +19,25 @@ RSpec.describe BetterBatch::Query do
   let(:table_name) { :the_table }
   let(:primary_key) { :the_primary_key }
   let(:columns) { %i[column_a column_b column_c] }
-  let(:unique_columns) { %i[column_b column_c] }
-  let(:column_types) do
+  let(:base_column_types) do
     {
+      primary_key => 'bigint',
       column_a: 'character varying(200)',
       column_b: 'bigint',
       column_c: 'text'
     }
   end
+  let(:column_types) { base_column_types }
+  let(:unique_columns) { %i[column_b column_c] }
+  let(:returning) { [primary_key] }
   let(:described_instance) do
     described_class.new(
       table_name:,
       primary_key:,
       columns:,
       column_types:,
-      unique_columns:
+      unique_columns:,
+      returning:
     )
   end
   let(:expected_query) { format_sql(raw_expected_query) }
@@ -43,8 +47,8 @@ RSpec.describe BetterBatch::Query do
 
     let(:raw_expected_query) do
       <<~SQL
-        select id from (
-          select the_table.the_primary_key as id, input.column_a, input.column_b, input.column_c, ordinal
+        select the_primary_key from (
+          select the_table.the_primary_key, input.column_a, input.column_b, input.column_c, ordinal
           from rows from (
             jsonb_to_recordset($1)
             as (column_a character varying(200), column_b bigint, column_c text)
@@ -56,6 +60,52 @@ RSpec.describe BetterBatch::Query do
     end
 
     it('returns the select query') { is_expected.to eq(expected_query) }
+
+    context 'more column types than needed are given' do
+      let(:column_types) { base_column_types.merge(unneeded: 'type') }
+      it('adds only used types to query') { is_expected.to eq(expected_query) }
+    end
+
+    context 'columns and column types are given in different order' do
+      let(:column_types) { base_column_types.to_a.reverse.to_h }
+      it('orders types correctly in query') { p column_types; is_expected.to eq(expected_query) }
+    end
+
+    context 'returning more columns' do
+      let(:returning) { [primary_key, :other_column] }
+      let(:raw_expected_query) do
+        <<~SQL
+          select the_primary_key, other_column from (
+            select the_table.the_primary_key, the_table.other_column, input.column_a, input.column_b, input.column_c, ordinal
+            from rows from (
+              jsonb_to_recordset($1)
+              as (column_a character varying(200), column_b bigint, column_c text)
+            ) with ordinality as input(column_a, column_b, column_c, ordinal)
+            left join the_table
+            using(column_b, column_c)
+          ) order by ordinal
+        SQL
+      end
+      it('returns the select query with more columns') { is_expected.to eq(expected_query) }
+    end
+
+    context 'no return specified' do
+      let(:returning) { nil }
+      let(:raw_expected_query) do
+        <<~SQL
+          select the_primary_key, column_a, column_b, column_c from (
+            select the_table.the_primary_key, input.column_a, input.column_b, input.column_c, ordinal
+            from rows from (
+              jsonb_to_recordset($1)
+              as (column_a character varying(200), column_b bigint, column_c text)
+            ) with ordinality as input(column_a, column_b, column_c, ordinal)
+            left join the_table
+            using(column_b, column_c)
+          ) order by ordinal
+        SQL
+      end
+      it('returns all columns') { is_expected.to eq(expected_query) }
+    end
   end
 
   describe '#upsert' do
@@ -64,7 +114,7 @@ RSpec.describe BetterBatch::Query do
     let(:raw_expected_query) do
       <<-SQL
         with selected as (
-          select the_table.the_primary_key as id, input.column_a, input.column_b, input.column_c, ordinal
+          select the_table.the_primary_key, input.column_a, input.column_b, input.column_c, ordinal
           from rows from (
             jsonb_to_recordset($1)
             as (column_a character varying(200), column_b bigint, column_c text)
@@ -77,16 +127,16 @@ RSpec.describe BetterBatch::Query do
           select distinct on (column_b, column_c)
             column_a, column_b, column_c, now() as created_at, now() as updated_at
           from selected
-          where id is null
-          returning id, column_b, column_c
+          where the_primary_key is null
+          returning the_primary_key, column_b, column_c
 
         )
         ,updated as (
           update the_table
           set column_a = selected.column_a, updated_at = now()
-          from selected where the_table.id = selected.id
+          from selected where the_table.the_primary_key = selected.the_primary_key
         )
-        select coalesce(selected.id, inserted.id) as id
+        select coalesce(selected.the_primary_key, inserted.the_primary_key)
         from selected left join inserted using(column_b, column_c)
         order by selected.ordinal
       SQL
@@ -99,7 +149,7 @@ RSpec.describe BetterBatch::Query do
       let(:raw_expected_query) do
         <<-SQL
           with selected as (
-            select the_table.the_primary_key as id, input.column_a, input.column_b, input.column_c, ordinal
+            select the_table.the_primary_key, input.column_a, input.column_b, input.column_c, ordinal
             from rows from (
               jsonb_to_recordset($1)
               as (column_a character varying(200), column_b bigint, column_c text)
@@ -112,10 +162,10 @@ RSpec.describe BetterBatch::Query do
             select distinct on (column_a, column_b, column_c)
               column_a, column_b, column_c, now() as created_at, now() as updated_at
             from selected
-            where id is null
-            returning id, column_a, column_b, column_c
+            where the_primary_key is null
+            returning the_primary_key, column_a, column_b, column_c
           )
-          select coalesce(selected.id, inserted.id) as id
+          select coalesce(selected.the_primary_key, inserted.the_primary_key)
           from selected left join inserted using(column_a, column_b, column_c)
           order by selected.ordinal
         SQL
