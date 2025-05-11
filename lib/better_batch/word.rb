712083f0827006aacded9f78a8a1304ddb6cec53
Tyler Hartland <tylerhartland7@gmail.com>
2025-05-11 11:42:57 -0500

    word class quotes when needed but still compares

---
+++ lib/better_batch/word.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'better_batch/postgresql_reserved'
+
+module BetterBatch
+  class Word
+    attr_reader :input
+
+    def initialize(input)
+      @input = input.to_s
+      downcased = input.downcase
+      @output = if POSTGRESQL_RESERVED.include?(downcased) || input != downcased
+                  "\"#{@input}\""
+                else
+                  @input
+                end
+    end
+
+    def to_s
+      @output
+    end
+
+    def ==(other)
+      case other
+      when self.class
+        input == other.input
+      when String
+        input == other
+      when Symbol
+        input == other.to_s
+      else
+        raise "Did not know how to compare to #{other.inspect}."
+      end
+    end
+  end
+end
---
+++ spec/better_batch/word_spec.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+require 'better_batch/word'
+
+RSpec.describe BetterBatch::Word do
+  let(:described_instance) { described_class.new(input) }
+
+  describe '#.to_s' do
+    subject { described_instance.to_s }
+
+    context 'matches a Postgres reserved word' do
+      let(:input) { 'order' }
+
+      it { is_expected.to eq('"order"') }
+    end
+
+    context 'includes capital letters' do
+      let(:input) { 'SomeThing' }
+
+      it { is_expected.to eq('"SomeThing"') }
+    end
+
+    context 'does not match a Postgres reserved word' do
+      let(:input) { 'something' }
+
+      it { is_expected.to eq('something') }
+    end
+
+    context 'input is a symbol' do
+      let(:input) { :something }
+
+      it { is_expected.to eq('something') }
+    end
+  end
+
+  describe '#==' do
+    subject { described_instance == other }
+
+    let(:other) { described_class.new(other_input) }
+
+    context 'same input' do
+      let(:input) { 'something' }
+      let(:other_input) { input }
+
+      it { is_expected.to be(true) }
+    end
+
+    context 'different input' do
+      let(:input) { 'something' }
+      let(:other_input) { 'something_else' }
+
+      it { is_expected.to be(false) }
+    end
+
+    context 'compared to string matching input' do
+      let(:input) { 'something' }
+      let(:other) { 'something' }
+
+      it { is_expected.to be(true) }
+    end
+
+    context 'compared to symbol matching input' do
+      let(:input) { 'something' }
+      let(:other) { :something }
+
+      it { is_expected.to be(true) }
+    end
+  end
+end
